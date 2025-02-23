a99e9dd714099bfd46004783fd46691dfbd29838
Valentina Orozco J <valenjaramillo08@gmail.com>
2025-02-23 17:59:49 -0500

    agregando metodos

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Prestamo.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Prestamo.java
@@ -7,8 +7,47 @@ public class Prestamo {
     public LocalDate fechaDevolucion;
     public Miembro miembro;
     public Libro libro;
+    public String codigoPrestamo;
 
     public Prestamo(){}
 
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
 
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Miembro getMiembro() {
+        return miembro;
+    }
+
+    public void setMiembro(Miembro miembro) {
+        this.miembro = miembro;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public String getCodigoPrestamo() {
+        return codigoPrestamo;
+    }
+
+    public void setCodigoPrestamo(String codigoPrestamo) {
+        this.codigoPrestamo = codigoPrestamo;
+    }
 }
